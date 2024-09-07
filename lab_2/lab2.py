a60d0f07a38252710c1e37effe136583da493f1a
Velesikamid <Velesikamid05082005@gmail.com>
2024-09-08 00:00:01 +0400

    Added new crawler

--- lab_2/lab2.py
+++ lab_2/lab2.py
@@ -1,10 +1,15 @@
 import argparse
-from icrawler.builtin import BingImageCrawler
+from icrawler.builtin import GoogleImageCrawler, BingImageCrawler, FlickrImageCrawler
 
 parser = argparse.ArgumentParser()
 parser.add_argument("keyword", type=str, help="search keyword (image class)")
 parser.add_argument("path", type=str, help="the path to the folder to save")
 args = parser.parse_args()
+path = args.path
+keyword = args.keyword
 
-google_crawler = BingImageCrawler(storage={"root_dir" : args.path})
-google_crawler.crawl(keyword=args.keyword, max_num=1000)
+google_crawler = GoogleImageCrawler(storage={"root_dir" : path})
+google_crawler.crawl(keyword=keyword, max_num=1000)
+
+bing_crawler = BingImageCrawler(storage={"root_dir" : path})
+bing_crawler.crawl(keyword=keyword, max_num=1000)
