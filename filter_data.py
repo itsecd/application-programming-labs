0eb73d43297b5ca18e14dfdcd05afb73188542b4
TimurMif <162575403+TimurMif@users.noreply.github.com>
2024-09-17 21:22:11 +0400

    Add file

---
+++ filter_data.py
@@ -0,0 +1,16 @@
+import re
+import argparse
+
+def count_data(filename: str, data: int) -> int:
+    with open(filename, "r") as file:
+        text = file.readlines()
+
+    pattern = r'[.]\d{4}'
+    count_data = 0
+    for i in range(len(text)):
+        if (re.search(pattern, text[i])):
+            date = re.findall(pattern, str(text[i]))
+            if (int(date[0][1:]) >= 2000):
+                count_data += 1
+                print(date[0][1:])
+    return count_data
