ef34a18d6d954b593ef7f5833eda1927df261a51
Sasha Sherwood <leksachirkova@gmail.com>
2024-09-18 23:51:45 +0400

    Entering the file name via the cmd.

--- lab1/main.py
+++ lab1/main.py
@@ -1,1 +1,10 @@
-print ("Hello world")
+import argparse
+parser = argparse.ArgumentParser()
+parser.add_argument('name_file', type=str, help='name file')
+args = parser.parse_args()
+name_file = args.name_file + ".txt"
+
+
+with open(name_file, "r", encoding='utf-8') as file:
+    text = file.read()
+print (text)
