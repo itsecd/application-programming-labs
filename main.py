a6d056252e78319c7fd78b6bb8aa3a50ccc73192
dmitrievsn <163391506+dmitrievsn@users.noreply.github.com>
2024-09-29 16:50:49 +0400

    Update main.py

--- main.py
+++ main.py
@@ -1,44 +1,62 @@
 import argparse
 import re
+
+
 def file_name()->str:
     """
-    Пользователь вводит название файла через командную строку
+    Вводит названия файла через командную строку.
     :return:
     """
     parser = argparse.ArgumentParser()
     parser.add_argument('file_name', type=str, help='Имя файла')
     args = parser.parse_args()
     return args.file_name
+
+
 def open_file(namefile: str) -> str:
     """
-    Считываем данные из файла
+    Считывание данных из файла по названию
     :param namefile:название файла
     :return:данные в виде строки из файла
     """
     with open(namefile,"r", encoding="utf-8") as file:
         text = file.read()
     return text
+
+
 def split_text(text: str) -> list[str]:
     """
-    Разделяем по анкетам
+    Разделение данных по анкетам
     :param text: наши данные в виде строки
     :return: список из анкет по отдельности
     """
     pattern=r'\d{1,2}\)'
     container=re.split(pattern,text)
     return container
-def profile_search(container: list):
+
+
+def profile_search(container: list) -> list[str]:
     """
-    Если находим нужные-печатаем
+    Поиск анкет, в которых номер телефона имеет код города 927.
     :param container: список анкет по отдельности
-    :return:ничего не возвращаем, печатаем анкеты, удовлетворяющие условию код города 927
+    :return:список анкет, удовлетворяющих условию
+    """
+    return [i for i in container if re.search(r'\+7 927',i)]
+
+
+def out_prof(container: list):
+    """
+    Вывод анкет
+    :param container:
+    :return: ничего не возвращаем, печатаем анкеты
     """
     c=0
     for i in container:
-        if re.search(r'\+7 927',i):
-            c+=1
-            print(str(c)+')')
-            print(i.strip())
+        c += 1
+        print(str(c) + ')')
+        print(i.strip())
+
+
 def main():
     """
     Используем наши функции
@@ -47,6 +65,9 @@ def main():
     filename=file_name()
     text=open_file(filename)
     container = split_text(text)
-    profile_search(container)
+    container=profile_search(container)
+    out_prof(container)
+
+
 if __name__ == "__main__":
     main()
