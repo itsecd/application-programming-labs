f556febef0eace71a5b18b3cc52887cf063f7fb5
Kirill <kirillstengauer@gmail.com>
2024-09-30 10:20:57 +0400

    ФИО: Штенгауэр Кирилл Алексеевич Номер группы: 6211 Номер лабораторной: 1 Номер варианта: 2 Краткое описание решаемой задачи: Согласно варианту 2 лабораторной 1 мне необходимо было написать программу, которая откроет файл, имя которого передается через аргумент командной строки, посредством модуля "argparse", и найдет в нем количество анкет людей родившихся в 21 веке. Краткое описание работы кода: Модуль __main__.py является главным модулем, который выполняет поставленную задачу. Чтобы его запустить, требуется указать аргументы командной строки: python main.py <path_to_file>

---
+++ laba1/main.py
@@ -0,0 +1,42 @@
+import codecs
+import argparse
+
+
+def create_parser():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('filename', type=str, help="Path to file")
+    return parser.parse_args().filename
+
+def read_file(filename:str)->list:
+    """
+    Reads a file, that was given to it
+    :param filename: the name of the file will be read
+    :return: the file converted to a list
+    """
+    file = codecs.open(filename, "r", "utf_8_sig")
+    text = file.readlines()
+    return text
+
+def age(txt: list)->int:
+    """
+    Сounts the number of people contained in the file that were born in the 21st century
+    :param txt: the text in which the calculation will be performed
+    :return: an amount of people that was born in 21st century
+    """
+    count: int = 0
+    new_list=[]
+    for i in range(4,len(txt),8):
+        new_list.append(txt[i])
+    for text in new_list:
+        if int(text[-5:-1])>1999:
+            count+=1
+    return count
+
+
+def main():
+    filename=create_parser()
+    text = read_file(filename)
+    print(age(text))
+
+if __name__ == "__main__":
+    main()
