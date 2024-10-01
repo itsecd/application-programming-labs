9047ceefc17324ed4a243e8a03d39e53ac1a6405
Bauntyy <t_chesnokov@bk.ru>
2024-10-01 22:22:51 +0400

    Add file

--- lab_1/lab_1.py
+++ lab_1/lab_1.py
@@ -2,6 +2,10 @@ import re
 import argparse
 
 def get_file_name() -> str:
+    """
+    Parses the file name from the command line arguments
+    :return:
+    """
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', type=str, help='name_of_file')
     args = parser.parse_args().filename
@@ -9,18 +13,33 @@ def get_file_name() -> str:
 
 
 def open_file(namefile:str) -> str:
+    """
+    Reading the contents of a file
+    :param namefile: The file name
+    :return: A string containing data from a file
+    """
     with open(namefile, 'r', encoding='utf_8') as file:
         text: str = file.read()
         return text
 
 
 def birthday(text:str) -> list:
+     """
+     Searches for the births dates of all people
+     :param text:The text that we read from the file
+     :return:Array with found dates of birth
+     """
      pattern = r'\d{2}.\d{2}.\d{4}'
      date = re.findall(pattern, text)
      return date
 
 
 def date_sort(date:list) -> str:
+    """
+    Looking for people born in the 21st century
+    :param date:Array with dates of birth
+    :return:The number of people born in 21
+    """
     count = 0
     for i in range(len(date)-1):
         year = int(date[i].split('.')[2])
