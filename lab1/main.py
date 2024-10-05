91c24c36ca4ccb16e976c1412c67cd3faea252b4
IvanDelibaltov <168005200+IvanDelibaltov@users.noreply.github.com>
2024-10-05 16:27:16 +0400

    Update main.py

--- lab1/main.py
+++ lab1/main.py
@@ -2,80 +2,67 @@ import argparse
 import re
 
 
-def get_file_name() -> str:
-
+def get_arguments() -> tuple:
     """
-    Extracts the file name from the command line arguments using the 'argparse' module
-    :return:the file name
+    Get filename and user name from command line arguments.
+    :return:  filename and user name
     """
-
     parser = argparse.ArgumentParser()
     parser.add_argument('filename', type=str, help='name of file')
-    args = parser.parse_args().filename
-    return args
+    parser.add_argument('name', type=str, help='User name')
+    args = parser.parse_args()
+    return args.filename, args.name.lower()
 
 
 def reading(filename: str) -> str:
     """
-    reads data from the file
+    Reads data from the file
     :param filename: name of file
-    :return:data from the file
+    :return: data from the file
     """
-
     with open(filename, 'r', encoding='utf-8') as file:
         return file.read()
 
 
-def namelist(data:str) -> list:
+def namelist(data: str) -> list:
     """
-    Create a list  of names and the substring "Name," then remove the substrings "Name."
+    Create a list of names and the substring "Имя,", then remove the substrings "Имя."
     :param data: data from file
     :return: list of names
     """
-    namelist = re.findall("Имя: \\w+", data)
-    namelist = [i.replace("Имя: ", "") for i in namelist]
+    namelist = re.findall(r"Имя:\s*(\w+)", data)
     namelist = [i.lower() for i in namelist]
     return namelist
 
+
 def input_name() -> str:
     """
-    The user enters a name.
-    :return: name by user
+    The user enters a name via command line arguments.
+    :return: name provided by the user
     """
-    inputname = input("Enter name: ")
-    inputname = inputname.lower()
-    return inputname
+    parser = argparse.ArgumentParser(description='Enter your name via command line.')
+    parser.add_argument('name', type=str, help='Your name')
+    args = parser.parse_args()
+    return args.name.lower()
+
 
 def countnames(namelist: list, input_name: str) -> int:
     """
-    Counting the number of  names
+    Counting the number of names
     :param namelist: list of names
     :param input_name: name by user
     :return: count of names
     """
-    count = 0
-    for string in namelist:
-        if string == input_name:
-            count += 1
-    return count
-
+    return namelist.count(input_name)
 
 
 def main():
-
-    filename = get_file_name()
+    filename, inputname = get_arguments()
     data = reading(filename)
     nl = namelist(data)
-    inputname = input_name()
     print(countnames(nl, inputname))
 
 
 if __name__ == "__main__":
-    """checking that the code executes as the main module"""
+    """Checking that the code executes as the main module"""
     main()
-
-
-
-
-
-    
