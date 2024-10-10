799e2ab42c56f24fcaaf22a89919543629c69d62
KuzminStanislav <a663ve@gmail.com>
2024-10-10 12:32:43 +0400

    lab1

--- lab1/lab1.py
+++ lab1/lab1.py
@@ -1,5 +1,4 @@
 import argparse
-import datetime
 import re
 
 def get_name_of_file() -> str:
@@ -12,46 +11,49 @@ def get_name_of_file() -> str:
     args = parser.parse_args()
     return args.filename
 
-
 def read_file(filename: str) -> str:
     """
     Getting data from file
     :param filename: filename
     :return: file data
     """
-    with open(filename, "r") as file:
-        return file.read()
+    try:
+        with open(filename, "r", encoding='utf-8') as file:
+            return file.read()
+    except FileNotFoundError:
+        print(f"Error: '{filename}' is not found.")
+        exit(1)
 
-
-def get_codes(data: str) -> list:
+def get_codes(data: list[str]) -> list[str]:
     """
     Compilating list of codes
     :param data: original data
     :return: list of codes
     """
-    pattern = re.findall('\\+7 927', data)
-    return pattern
+    pattern_for_split = r'\d+\)\n'
+    split_data = re.split(pattern_for_split, data, maxsplit=0)
+    pattern = r'\+7 927\d{7}'
+    found_data = list()
+    for codes in split_data:
+        if re.search(pattern, codes):
+            found_data.append(codes)
+    return found_data
 
-
-def count_codes(pattern: list) -> int:
+def print_codes(found_data: list[str]) -> None:
     """
-    Counting codes
-    :param pattern: list of codes
-    :return: Count of numbers
+    Printing list
+    :param found_data: final list of codes
+    :return: None
     """
-    count = 0
-    for code in pattern:
-        count += 1
-    return count
-
+    for codes in found_data:
+        print(codes)
+    print()
 
 def main():
     filename = get_name_of_file()
-    data = read_file(filename)
-    codes = get_codes(data)
-    count_of_codes = count_codes(codes)
-    print(count_of_codes)
-
+    data: str = read_file(filename)
+    found_data = get_codes(data)
+    print_codes(found_data)
 
 if __name__ == "__main__":
     main()
