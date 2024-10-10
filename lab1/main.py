c833b3f5a822fdc4ce27ef7320e47127c071ea07
ilyanikoniko <iliya05163@gmail.com>
2024-10-10 12:46:12 +0300

    Add

--- lab1/main.py
+++ lab1/main.py
@@ -1,15 +1,17 @@
-import re
 import argparse
+import re
+
 
 def get_file_name() -> str:
     """
     считывание имени файл из командной строки
-    :return:имя файла
+    :return: имя файла
     """
     parser = argparse.ArgumentParser()  # создание экземпляра парсера
     parser.add_argument('filename', type=str, help='name of file')  # добавление позиционного аргумента командной строки
     args = parser.parse_args().filename  # парсинг аргументов
     return args
+
 
 def read_file(namefile: str) -> str:
     """
@@ -20,24 +22,26 @@ def read_file(namefile: str) -> str:
         - пустой список если есть ошибки, связанные с файлом
     """
     try:
-      with open(namefile, mode='r', encoding="utf-8") as file:
-        text = file.read()
-        return text
+        with open(namefile, mode='r', encoding="utf-8") as file:
+            text = file.read()
+            return text
     except Exception as e:
-      print(f"Error: {e}")
-      return []
+        print(f"Error: {e}")
+        return []
+
 
 def split_questionnaires(lines: str) -> list[str]:
     """
     Разделяет текст анкет на отдельные анкеты.
-    :param lines:Строка, содержащая текст анкет
+    :param lines: Строка, содержащая текст анкет
     :return: split_text
     """
     pattern = r'\d+\)\s*'
-    split_text = re.split(pattern, lines,maxsplit=0)
-    return  split_text
+    split_text = re.split(pattern, lines, maxsplit=0)
+    return split_text
 
-def search_by_template( split_text: list[str]) -> list[str]:
+
+def search_by_template(split_text: list[str]) -> list[str]:
     """
     Ищет анкеты по шаблону
     :param split_text: список анкет
@@ -45,10 +49,11 @@ def search_by_template( split_text: list[str]) -> list[str]:
     """
     pattern = r'Фамилия: Иванов[а]?'
     found_list = list()
-    for questionnaire in  split_text:
+    for questionnaire in split_text:
         if re.search(pattern, questionnaire):
-          found_list.append(questionnaire)
+            found_list.append(questionnaire)
     return found_list
+
 
 def print_questionnaire(found_list: list[str]) -> None:
     """
@@ -60,12 +65,14 @@ def print_questionnaire(found_list: list[str]) -> None:
         print(questionnaire)
     print()
 
+
 def main():
     namefile = get_file_name()
     text = read_file(namefile)
     questionnaires = split_questionnaires(text)
     ivanovs_list = search_by_template(questionnaires)
     print_questionnaire(ivanovs_list)
+
 
 if __name__ == "__main__":
     main()
