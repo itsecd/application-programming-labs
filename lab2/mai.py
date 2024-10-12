4d75c349aa68fdfe386a801e92ab78233cf5eb43
илья <Oksimonov@mail.ru>
2024-10-13 01:23:41 +0300

    добавляем csv файл

--- lab2/mai.py
+++ lab2/mai.py
@@ -1,1 +1,19 @@
-print("Djааааааа")
+import csv
+
+# данные для записи
+data = [
+    ["Имя", "Возраст", "Город"],  # Заголовки
+    ["Алексей", 30, "Москва"],
+    ["Мария", 25, "Санкт-Петербург"],
+    ["Иван", 22, "Екатеринбург"]
+]
+
+# запись данных в CSV файл
+with open('data.csv', mode='w', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerows(data) # запись всех строк
+
+# дозапись в файл
+with open('data.csv', mode='a', newline='', encoding='utf-8') as file:
+    writer = csv.writer(file)
+    writer.writerow(["Ирина", 33, "Сочи"])
